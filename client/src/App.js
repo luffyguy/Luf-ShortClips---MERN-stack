d52e8b81852215903b92b46e87be2f9617c3821c
Luffyguy <harshtanwar25@gmail.com>
2021-05-18 15:20:12 +0530

    Header, Video Posiitoning Fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Video from "./components/Video/Video";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app-Container">
+      <Header />
+      <div className="app">
+        <div className="app__video">
+          <Video />
+          <Video />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Video/VideoFooter/VideoFooter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./VideoFooter.css"
+
+const VideoFooter = () => {
+    return (
+        <div className = "videoFooter">
+            <div className="videoFooter__text">
+                <h3>Name of user</h3>
+                <p>Discription of video</p>
+            </div>
+        </div>
+    )
+}
+
+export default VideoFooter
