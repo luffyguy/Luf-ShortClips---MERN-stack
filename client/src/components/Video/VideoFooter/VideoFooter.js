dfb81266dd91532be2c441e438725bdc66fa38ad
Luffyguy <harshtanwar25@gmail.com>
2021-05-19 11:10:31 +0530

    Updated Gui

--- client/src/components/Video/VideoFooter/VideoFooter.js
+++ client/src/components/Video/VideoFooter/VideoFooter.js
@@ -5,7 +5,7 @@ const VideoFooter = ({ userName, description }) => {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
-        <h3>{userName}</h3>
+        <h3>@{userName}</h3>
         <p>{description}</p>
       </div>
     </div>
